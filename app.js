6695479274cbc295a7aecf4cd01327e0d978ebbe
liban13130 <nabil-13130@hotmail.fr>
2023-04-30 21:50:38 +0200

    js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// API ENDPOINT : `https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${searchInput}`
+
+
 const form     = document.querySelector('form')
 const input    = document.querySelector('input')
 const errorMsg = document.querySelector('.error-msg')
@@ -6,7 +9,6 @@ form.addEventListener('submit', handleSubmit)
 
 function handleSubmit(e){
     e.preventDefault()
-    console.log(e);
 
     if(input.value === ""){
         errorMsg.textContent = "Oops... Veuillez remplir l'input.";
@@ -15,4 +17,38 @@ function handleSubmit(e){
         errorMsg.textContent = "";
         wikiApiCall(input.value)
     }
+}
+
+async function wikiApiCall(searchInput){
+
+    const response = await fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${searchInput}`)
+
+    const data = await response.json()  //Pour analyser le corps de la requête (le body)
+    console.log(data);
+
+    createCards(data.query.search)
+}
+
+const resultDisplay = document.querySelector('.results-display')
+
+function createCards(data){
+    if(!data.length){
+        errorMsg.textContent = "Oops... Aucun résultat.";
+        return;
+    }
+    data.forEach(el => {
+        const url = `https://en.wikipedia.org/?curid=${el.pageid}`
+        const card = document.createElement('div'); //Creation de la card
+        card.className = 'result-item'; //On lui ajoute une classe
+        card.innerHTML = `
+        <h3 class="result-title">
+            <a href="${url}" target ="_blank">${el.title}</a>
+        </h3>
+        <a href="${url}" class="result-link" target ="_blank">${url}<a/>
+        <span class="result-snippet">${el.snippet}</span>
+        <br>
+        ` //On lui a ajouté son contenu interne 
+
+        resultDisplay.appendChild(card) // On ajoute cette card (card) a .results-display. Ce sera l'enfant de .results-display
+    });
 }
